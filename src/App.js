b03a122a9556c3af39c0909214b602d9e98194ec
phungphuc <phungthienphuc18@gmail.com>
2019-07-08 17:54:28 +0700

    format code

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-
-  state = {users: []}
+  state = {users: []};
 
   componentDidMount() {
     fetch('/users')
       .then(res => res.json())
-      .then(users => this.setState({ users}));
+      .then(users => this.setState({users}));
   }
 
   render() {
     return (
       <div className="App">
         <h1>Users</h1>
-        {
-          this.state.users.map(user => 
-            <div key={user.id}>{user.username}</div>
-          )
-        }
+        {this.state.users.map(user => (
+          <div key={user.id}>{user.username}</div>
+        ))}
+        <h2>Trying to put the context to see if the submodule work</h2>
       </div>
-    )
+    );
   }
-
 }
 export default App;
